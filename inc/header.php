aa4c20dabb6fac8f85eec00e5e857280fc44589d
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-12-01 14:34:45 +0600

    footer

--- inc/header.php
+++ inc/header.php
@@ -18,7 +18,7 @@ require_once "config/function.php";
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>FutureLeaders Admin</title>
+  <title>Mudur Kotha by Dr Sakhawat</title>
   <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
   <link rel="stylesheet" href="assets/css/style.css">
