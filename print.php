a9122306a8b5dd30f3ab3bb6596ca02180dd835d
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-12-04 02:10:15 +0600

    Update print.php

--- print.php
+++ print.php
@@ -391,7 +391,7 @@ require_once "config/db_conn.php";
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script>
-        // window.print();
+        window.print();
     </script>
 </body>
 
